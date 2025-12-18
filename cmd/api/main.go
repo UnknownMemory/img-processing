eed65be5f2e4495df53b7ebf6648b8fce45996c4
UnknownMemory <jeremyschneider21@gmail.com>
2025-12-18 23:30:57 +0100

    chore: init server & db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+const version = "0.1.0"
+
+type config struct {
+	port int
+	mode string
+	db   struct {
+		dsn string
+	}
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+	db     *pgxpool.Pool
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var cfg config
+
+	cfg.port, _ = strconv.Atoi(os.Getenv("PORT"))
+	cfg.mode = os.Getenv("MODE")
+	cfg.db.dsn = os.Getenv("DB_DSN")
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+		db:     db,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/healthcheck", app.healthcheckHandler)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.mode, server.Addr)
+	err = server.ListenAndServe()
+	logger.Fatal(err)
 }
