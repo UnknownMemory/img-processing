19f672f2268e08296eb96f2c0550af7cb90b8298
UnknownMemory <jeremyschneider21@gmail.com>
2025-12-24 15:18:07 +0100

    chore(db): add scripts to set goose environment variables

---
+++ goose.env.ps1
@@ -0,0 +1,3 @@
+$Env:GOOSE_DRIVER="postgres"
+$Env:GOOSE_DBSTRING="postgres://postgres:postgres@localhost:5432/img_process"
+$Env:GOOSE_MIGRATION_DIR="./sql/migrations"
---
+++ goose.env.sh
@@ -0,0 +1,3 @@
+export GOOSE_DRIVER="postgres"
+export GOOSE_DBSTRING="postgres://postgres:postgres@localhost:5432/img_process"
+export GOOSE_MIGRATION_DIR="./sql/migrations"
