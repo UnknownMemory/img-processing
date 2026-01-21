0d5a47f823d776a1ff9dce2bce4d759c47b668b1
UnknownMemory <jeremyschneider21@gmail.com>
2026-01-21 22:36:09 +0100

    feat(api): handle image upload and valid mime type

--- internal/api/images.go
+++ internal/api/images.go
@@ -2,8 +2,44 @@ package api
 
 import (
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func (app *Application) uploadImageHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	err := r.ParseMultipartForm(50 << 20)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "An error occurred during the upload", http.StatusBadRequest)
+		return
+	}
+
+	file, handler, err := r.FormFile("image-upload")
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "An error occurred during the upload", http.StatusBadRequest)
+		return
+	}
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "An error occurred during the upload", http.StatusBadRequest)
+		return
+	}
+	mimeType := http.DetectContentType(buffer)
+	if !strings.Contains(mimeType, "image/") {
+		app.logger.Println(err)
+		http.Error(w, "Invalid MIME type", http.StatusBadRequest)
+		return
+	}
+
+	fileData := map[string]string{"filename": handler.Filename, "size": strconv.FormatInt(handler.Size, 10)}
+
+	err = app.writeJSON(w, http.StatusOK, fileData, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -5,9 +5,12 @@ import "net/http"
 func (app *Application) Routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/v1/healthcheck/", app.healthcheckHandler)
+
 	mux.HandleFunc("POST /api/v1/users/register", app.registerUserHandler)
 	mux.HandleFunc("POST /api/v1/users/login", app.loginUserHandler)
+
 	mux.HandleFunc("GET /api/v1/token/refresh", app.refreshTokenHandler)
+
 	mux.Handle("POST /api/v1/images/upload", app.authMiddleware(http.HandlerFunc(app.uploadImageHandler)))
 	return app.secureHeaders(mux)
 }
